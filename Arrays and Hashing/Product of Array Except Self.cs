e64ede09c5842e378f302b5f205ec082089265bb
Rhyth Rodrigues <8902.rhyth.secompsa@gmail.com>
2026-01-03 16:13:57 +0530

    Solved Product of Array Except Self

---
+++ Arrays and Hashing/Product of Array Except Self.cs
@@ -0,0 +1,31 @@
+/*
+Product except self =
+(product of all elements to the LEFT of i) ×
+(product of all elements to the RIGHT of i)
+*/
+class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+
+        int[] res = new int[nums.Length];
+
+
+
+        int pre = 1, post = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            res[i] = pre;
+            pre = pre * nums[i];
+        }
+        // 1 2 3 4 --> 1 1 2 6 Prefix             1 2 3 4 --> 24 12 4 1 Postfix
+        // 1 1 2 6 --> 24 12 8 6      
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            res[i] = res[i] * post;
+            post = post * nums[i];
+
+        }
+        return res;
+    }
+}
